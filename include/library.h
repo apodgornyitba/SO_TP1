ee2e3b0d329d4012d23a3f6ac5c9a5fffcd3094d
apodgornyitba <apodgorny@itba.edu.ar>
2022-04-02 18:29:43 -0300

    Semaphore and Shared Memory handlers

--- include/library.h
+++ include/library.h
@@ -1,1 +1,54 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#define _DEFAULT_SOURCE
+#define  _GNU_SOURCE
+
+#include <errno.h>
+#include <fcntl.h>           /* For O_* constants */
+#include <semaphore.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/select.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define ERROR_CODE -1
+#define ERROR_MSG 100
+
+#define INIT_VAL_SEM 0
+#define SEM_NAME "/semaphore"
+#define SHM_NAME "/shMemory"
+
+#define STDIN 0
+#define STDOUT 1
+
+#define READ_FD 0
+#define WRITE_FD 1
+
+#define JUMP 1024
+#define BUFFER_SIZE 1024
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) > (b) ? (b) : (a))
+
+void errorHandler(const char *);
+
+sem_t * semOpen(const char *name, int oflag, mode_t mode, unsigned int value);
+void semPost(sem_t *);
+void semWait(sem_t *);
+void semClose(sem_t *);
+void semUnlink();
+
+int openSM(const char* name, int oflag, mode_t mode);
+void unmapSM(void *, int );
+void unlinkSM();
+void truncateSM(int fd, off_t lenght);
+
+void setBuffer(FILE * ,size_t );
+
+#endif
--- library.c
+++ library.c
@@ -1,3 +1,83 @@
-int main{
-    
+#include "library.h"
+
+/*** *** *** ERROR HANDLING *** *** *** *** *** *** ***/
+
+void errorHandler(const char *errorMsg) {
+    char finalMsg[ERROR_MSG] = {0};
+    strcat(finalMsg,errorMsg);
+    strcat(finalMsg,"\n");
+    perror(finalMsg);
+    exit(EXIT_FAILURE);
+}
+
+
+/*** *** *** SEMAHPHORE HANDLING *** *** *** *** *** *** ***/
+
+sem_t * semOpen(const char *name, int oflag, mode_t mode, unsigned int value){
+    sem_t * sem;
+    if ((sem = sem_open(SEM_NAME, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, INIT_VAL_SEM)) == SEM_FAILED) {
+        errorHandler("Error opening semaphore (app)");
+    }
+    return sem;
+}
+
+void semPost(sem_t *sem) {
+    if(sem_post(sem) == ERROR_CODE) {
+        errorHandler("Error in sem_post");
+    }
+}
+
+void semWait(sem_t * sem) {
+  if(sem_wait(sem) == ERROR_CODE) {
+      errorHandler("Error: sem_wait has failed");
+  }
+}
+
+void semClose(sem_t * sem) {
+    if(sem_close(sem) == ERROR_CODE) {
+        unlinkSemaphore();
+        errorHandler("Error closing semaphore");
+    }
+}
+
+void semUnlink() {
+    if(sem_unlink(SEM_NAME) == ERROR_CODE) {
+        errorHandler("Error unlinking semaphore");
+    }
+}
+
+
+/*** *** *** SHARED MEMORY HANDLING *** *** *** *** *** *** ***/
+
+int openSM(const char* name, int oflag, mode_t mode){
+    int fd;
+    if ((fd = shm_open(name, oflag, mode)) == ERROR_CODE) {
+        errorHandler("Error opening shared memory (app)");
+    }
+    return fd;
+}
+
+void unmapSM(void * memory, int size) {
+    if(munmap( memory,size) == ERROR_CODE) {
+        errorHandler("Error unmapping shared memory");
+    }
+}
+
+
+void unlinkSM() {
+    if(shm_unlink(SHM_NAME)==ERROR_CODE) {
+        errorHandler("Error unlinking shared memory");
+    }
+}
+
+void truncateSM(int fd, off_t lenght){
+    if (ftruncate(fd, lenght) == ERROR_CODE) {
+        errorHandler("Error setting size to shared memory (app)");
+    }
+}
+
+void setBuffer(FILE * stream,size_t size) {
+        if (setvbuf(stream, NULL, _IONBF, size) != 0) {
+        errorHandler("Error setting buffer in process application");
+    }
 }
